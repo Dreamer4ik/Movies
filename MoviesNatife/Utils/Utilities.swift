d0a187d0f4b3789cf817b8789d6c882bcef7fe6d
Ivan Potapenko <dreamer4ik@gmail.com>
2023-10-23 18:57:18 +0300

    Set up project

---
+++ MoviesNatife/Utils/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  MoviesNatife
+//
+//  Created by Ivan Potapenko on 23.10.2023.
+//
+
+import UIKit
+
+final class Utilities {
+    static func configureNavBar(vc: UIViewController) {
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.backgroundColor = .white
+        vc.navigationItem.standardAppearance = barAppearance
+        vc.navigationItem.scrollEdgeAppearance = barAppearance
+    }
+}
+
